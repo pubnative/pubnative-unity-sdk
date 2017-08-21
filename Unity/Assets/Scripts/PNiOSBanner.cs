ca2887cccdf30d689f839758798b779d250c256f
Can Soykarafakılı <can.soykarafakili@gmail.com>
2017-08-21 17:22:37 +0200

    Base codes are added for the new structure.

--- Unity/Assets/Scripts/PNiOSBanner.cs
+++ Unity/Assets/Scripts/PNiOSBanner.cs
@@ -2,19 +2,25 @@
 
 public class PNIOSBanner : PNBanner
 {
+	[System.Runtime.InteropServices.DllImport("__Internal")]
+	extern static public void load(string objectName, string appToken, string placement);
 	public override void Load ()
 	{
-		
+		 load(this.gameObject.name, appToken, placement);
 	}
 
+	[System.Runtime.InteropServices.DllImport("__Internal")]
+	extern static public void show(int positon);
 	public override void Show (Position position)
 	{
 		int positionValue = (int)position;
+		show(positionValue);
 	}
 
+	[System.Runtime.InteropServices.DllImport("__Internal")]
+	extern static public void hide ();
 	public override void Hide ()
 	{
-		
+		hide();
 	}
 }
-
