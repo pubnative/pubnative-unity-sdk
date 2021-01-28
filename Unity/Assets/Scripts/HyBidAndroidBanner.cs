4ad7cbd2b7db8a9f2e8215a368f60f9e9d49118e
Abanoub <abanoub.cww@gmail.com>
2021-01-28 11:45:52 +0200

    Update HybidAndroidBanner.cs
    remove unused constants

--- Unity/Assets/Scripts/HyBidAndroidBanner.cs
+++ Unity/Assets/Scripts/HyBidAndroidBanner.cs
@@ -8,8 +8,6 @@ public class HyBidAndroidBanner : HyBidBanner {
     
     protected const string ANDROID_CLASS = "net.pubnative.unityplugin.HyBidAdViewWrapper";
 	protected const string LOAD_METHOD = "load";
-	protected const string HIDE_METHOD = "hide";
-	protected const string SHOW_METHOD = "show";
 
 	protected AndroidJavaObject layoutWrapper;
 
