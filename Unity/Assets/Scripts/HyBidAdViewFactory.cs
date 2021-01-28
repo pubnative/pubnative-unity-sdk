c15f7f0b6233761b1072b221d69550bae18387a5
Abanoub <abanoub.cww@gmail.com>
2021-01-28 12:02:58 +0200

    Update HybidAdViewFactory
    update HybidAdViewFacotry with new classes

--- Unity/Assets/Scripts/HyBidAdViewFactory.cs
+++ Unity/Assets/Scripts/HyBidAdViewFactory.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class HyBidAdViewFactory
 {
     public static HyBidBanner createHyBidAdView(MonoBehaviour parent) 
